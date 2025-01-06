93908ddad7ed56fdc409a51b719d1b78992ad4f4
Ubuntu <ubuntu@ip-172-31-64-248.ec2.internal>
2025-01-06 04:07:51 +0000

    docker detach mode

--- run/start.sh
+++ run/start.sh
@@ -1,3 +1,7 @@
+#stop container if present and remove it 
+docker stop refresh
+docker rm refresh
+
 #clean any ols image
 docker image prune -f
 
@@ -5,5 +9,5 @@ docker image prune -f
 docker build -t refresh .
 
 #start the application
-docker run --name=refresh --hostname=refresh -p 8000:8000 -e PORT=8000 -v "/home/ubuntu/repo/data:/app/db/chroma_db_jobs" --network IK_Net --rm -it refresh
+docker run --name=refresh -d --hostname=refresh -p 8000:8000 -e PORT=8000 -v "/home/ubuntu/repo/data:/app/db/chroma_db_jobs" --network IK_Net --restart always -it refresh
 
