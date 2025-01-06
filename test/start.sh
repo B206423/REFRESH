d83e8c5b930832942f6e1447cb03ea31016bc581
Ubuntu <ubuntu@ip-172-31-64-248.ec2.internal>
2025-01-06 01:31:43 +0000

    added docker network & image cleanup

--- test/start.sh
+++ test/start.sh
@@ -1,4 +1,9 @@
-sudo docker build -t refresh .
-#sudo docker run --name=refresh --hostname=refresh -p 8000:8000 -e PORT=8000 -v "chromadb:/app/db/chroma_db_jobs" --rm -it refresh
-sudo docker run --name=refresh --hostname=refresh -p 8000:8000 -e PORT=8000 -v "/home/sgadm/repo/data/chroma_db_jobs:/app/db/chroma_db_jobs" --rm -it refresh
+#clean any ols image
+docker image prune -f
+
+#build application 
+docker build -t refresh .
+
+#start the application
+docker run --name=refresh --hostname=refresh -p 8000:8000 -e PORT=8000 -v "/home/ubuntu/repo/data:/app/db/chroma_db_jobs" --network IK_Net --rm -it refresh
 
